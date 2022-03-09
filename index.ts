297b01c2f2e3286ade64560b887d72af18307290
Ignacy Litwin <ignacy.litwin@microsoft.wsei.edu.pl>
2022-03-09 13:11:42 +0100

    pierwsza instrukcja bez finiszu

---
+++ index.ts
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+    const num1 = req.query.num1
+    const num2 = req.query.num2
+    const operations = req.query.operations
+  res.send(`${num1} ${num2} ${operations}`)
+})
+app.listen(3000)
